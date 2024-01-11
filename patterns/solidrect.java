22cb7ab0c0dbb6d6486f264e66e09a7d387a769a
kumkumshh <kumkumsharma227@gmail.com>
2024-01-11 22:47:06 +0530

    completed

--- patterns/solidrect.java
+++ patterns/solidrect.java
@@ -1,14 +1,12 @@
 package patterns;
-import java.util.*;
 
 public class solidrect {
     public static void main(String[] args) {
         
         // solid rectangle 
 
-        Scanner s1 = new Scanner(System.in);
-        int n = s1.nextInt(); //for the row
-        int m = s1.nextInt(); //for the column
+        int n = 4;//for the row
+        int m = 5;//for the column
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
                 System.out.print("* ");
