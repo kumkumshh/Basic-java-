16f28b76efcf78b8cd80aaeb7f362c5b93508f79
kumkumshh <kumkumsharma227@gmail.com>
2024-01-11 22:29:55 +0530

    file updated

--- patterns/invertedhalfpyramidwithnum.java
+++ patterns/invertedhalfpyramidwithnum.java
@@ -1,5 +1,15 @@
 package patterns;
 
 public class invertedhalfpyramidwithnum {
-    
+    public static void main(String[] args) {
+        
+        //inverted half pyramid with numbers
+        int n=5;
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 }
--- patterns/rotatedhalfpyramid.java
+++ patterns/rotatedhalfpyramid.java
@@ -6,6 +6,20 @@ public class rotatedhalfpyramid {
         //inverted half pyramid
         // rotated by 180 deg
 
-        
+        int n=4;
+
+        for(int i=1; i<=n; i++){
+            //for the spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            //for the stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
     }
+     }
+        
 }
