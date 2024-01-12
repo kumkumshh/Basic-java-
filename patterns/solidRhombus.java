cbda83af9433a5acc6d7d354755de3c44e2b11f9
kumkumshh <kumkumsharma227@gmail.com>
2024-01-12 21:46:34 +0530

    file added

--- patterns/solidRhombus.java
+++ patterns/solidRhombus.java
@@ -9,12 +9,13 @@ public class solidRhombus {
         //solid rhombus
         int n=5;
 
-
-        for (int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++){
+            //spaces
             for(int j=1; j<=n-i; j++){
                 System.out.print(" ");
             }
-            for(int j=1; j<=i; j++){
+            //stars
+            for(int j=1; j<=5; j++){
                 System.out.print("*");
             }
             System.out.println();
